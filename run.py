b1acfec0ef02032e7850aa6482e38d83221def3b
Marcelo Machado <mmachado1711@gmail.com>
2024-03-08 15:06:11 -0300

    ok

--- run.py
+++ run.py
@@ -1,11 +1,4 @@
-import streamlit as st
-from streamlit_autorefresh import st_autorefresh
 from st_pages import Page, show_pages
-
-
-refresh_count = st_autorefresh(interval=10 * 60 * 1000, key="mainrefresh", limit=None)
-
-
 
 def run():
 
@@ -15,7 +8,5 @@ def run():
             Page("./pages/Laboratorio.py", "Laboratorio", "👓"),       
         ]
     )
-    page_name = st.sidebar.radio("Navegação", list(show_pages.keys()))
-    show_pages[page_name]()
 
 run()
